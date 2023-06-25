ba6a678e8106268784f1ff1d07a3c28f941ae110
abdulqadir876 <hindiseconsulting@gmail.com>
2023-06-25 18:43:56 +0300

    added video player

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,15 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import whizCyberBanner from "../img/whiz-cyber-banner.png";
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useAnimation } from "framer-motion";
+
+import { Dialog, Transition } from "@headlessui/react";
+import ReactPlayer from "react-player";
 
 const Hero = () => {
   const { ref, inView } = useInView();
@@ -29,6 +32,18 @@ const Hero = () => {
       });
     }
   });
+
+  //
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  //
 
   const heroStyle = {
     backgroundImage:
@@ -65,7 +80,7 @@ const Hero = () => {
           <div className="col-span-1 z-48">
             <motion.h1
               animate={animation}
-              className="text-white md:text-[70px] mt-16 text-[37px] font-bold md:leading-[70px] leading-[40px] md:text-left text-center"
+              className="text-white md:text-[70px] md:t-0 mt-16 text-[37px] font-bold md:leading-[70px] leading-[40px] md:text-left text-center"
             >
               Everything Starts{" "}
               <span className="animate-text bg-gradient-to-r from-[#2D76E7] to-[#802FD7] via-[#511b64] bg-clip-text text-transparent">
@@ -74,6 +89,56 @@ const Hero = () => {
               For Your Security
             </motion.h1>
           </div>
+          {/* Diolog */}
+          <div className="my-4">
+            <div className="inset-0 flex justify-left md:justify-center">
+              <button
+                type="button"
+                onClick={openModal}
+                className="rounded-md bg-gradient-to-l to-primary from-secondary hover:to-secondary hover:from-primary bg-opacity-20 px-4 py-2 font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+              >
+                Watch Video
+              </button>
+            </div>
+         
+            <Transition appear show={isOpen} as={Fragment}>
+              <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 m-10">
+                  <div className="flex h-full items-center justify-center p-4 text-center">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0 scale-95"
+                      enterTo="opacity-100 scale-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100 scale-100"
+                      leaveTo="opacity-0 scale-95"
+                    >
+                      <Dialog.Panel className=" bg-red-900 flex justify-center transform  rounded-2xl  p-6 shadow-xl transition-all">
+                        <div className="mt-2">
+                          <ReactPlayer url="https://vimeo.com/839465389" controls />
+                        </div>
+
+                      </Dialog.Panel>
+                    </Transition.Child>
+                  </div>
+                </div>
+              </Dialog>
+            </Transition>
+          </div>
+          {/* End Diolog */}
         </div>
         {/* Image */}
         <motion.div animate={animation}>
